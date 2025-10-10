0203af32e498fa7fab027cea0b7a6d0bf30d4af6
erisoff <erison.guimaraes@sga.pucminas.br>
2025-10-10 19:33:55 -0300

    add nexum app

---
+++ src/nexumApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nexumApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
