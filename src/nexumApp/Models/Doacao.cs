35fea4cc87b56101c51d618e5366fef23cfc8d4c
Mariana Gressi <marianatgressi@gmail.com>
2025-10-12 21:48:08 -0300

    Criação de  Modelo de Claases - Doacao e Meta

---
+++ src/nexumApp/Models/Doacao.cs
@@ -0,0 +1,21 @@
+﻿namespace nexumApp.Models
+{
+    public class Doacao
+    {
+        public int IdDoacao { get; set; }
+        public string Tipo { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+        public int Quantidade { get; set; }
+        public string NomeRazaoSocial { get; set; }
+        public string Email { get; set; }
+        public int IdOngDestinataria { get; set; }
+        public int IdOngDoadora { get; set; }
+
+        // Chave Estrangeira e Propriedade de Navegação:
+        // Isso representa o relacionamento com a Meta.
+        // Uma Doação pertence a exatamente uma Meta.
+        public int IdMeta { get; set; }
+        public virtual Meta Meta { get; set; }
+    }
+}
---
+++ src/nexumApp/Models/Meta.cs
@@ -0,0 +1,17 @@
+﻿namespace nexumApp.Models
+{
+    public class Meta
+    {
+        public int IdMeta { get; set; }
+        public string Status { get; set; }
+        public int IdRecurso { get; set; } 
+        public string Recurso { get; set; }
+        public int QuantidadeNecessaria { get; set; }
+        public int QuantidadeReservada { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+
+        // Uma Meta pode ter várias Doações.
+        // Isso representa o relacionamento (0..*) do diagrama.
+        public virtual ICollection<Doacao> Doacoes { get; set; }
+    }
+}
