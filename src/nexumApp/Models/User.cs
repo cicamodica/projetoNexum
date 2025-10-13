6b5dc948faea9f874c972e52f6cf4176401116ac
erisoff <erison.guimaraes@sga.pucminas.br>
2025-10-13 14:11:58 -0300

    customizando modelo de registro

---
+++ src/nexumApp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace nexumApp.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
--- src/nexumApp/Program.cs
+++ src/nexumApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using nexumApp.Data;
+using nexumApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
