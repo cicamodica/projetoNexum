685008a1baf6f2b460beccce7b8db483b321a5d7
marciamdrmarques <marcia.maria@sga.pucminas.br>
2025-10-24 20:36:07 -0300

    add modelo da tabela Inscricoes

---
+++ src/nexumApp/Models/Inscricoes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nexumApp.Models
+{
+    public class Inscricoes
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(3)]
+        public int IdMeta { get; set; }
+        public int IdCandidato { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataInscricao { get; set; }
+
+    }
+}
