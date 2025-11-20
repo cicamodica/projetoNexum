f53cb5c1040cece88ac5bd86e433adc8ce7c73a1
Cecilia Modica <ceciliacmodica@gmail.com>
2025-11-20 17:26:19 -0300

    Corrigindo cadastro

--- src/nexumApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/nexumApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -164,7 +164,7 @@ namespace nexumApp.Areas.Identity.Pages.Account
                     await _signInManager.SignInAsync(user, isPersistent: true);
                     if(CadastroAction == "Cadastrar")
                     {
-                        return RedirectToAction("Details", "Ongs", new { id = ong.Id });
+                        return RedirectToAction("Details", "Ongs", new { area = "", id = ong.Id });
                     }
                     return RedirectToAction("Create", "Filials", new { area = "" });
                 }
